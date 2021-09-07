180ec9d3fa924aefac4361143fad7c1136d005d4
raj-laddha <laddharaj01@gmail.com>
2021-09-07 12:55:59 +0530

    file meta data

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer({dest: 'public/files'});
+const fs = require('fs');
 require('dotenv').config()
 
 var app = express();
@@ -12,7 +15,20 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 
+  fs.unlink(req.file.path, (err) => {
+    if (err) return console.log(err);
+
+    console.log(req.file.originalname + ' Successfully Deleted');
+  });
+
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
